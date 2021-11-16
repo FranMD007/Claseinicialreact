4d977276c74dd12ce3e052e9771b0a89a7471938
FranMD007 <fran.2305@utp.edu.co>
2021-11-15 20:10:34 -0500

    comit imagen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src="https://cn.i.cdn.ti-platform.com/content/1228/new-looney-tunes/showpage/co/showpicker-2.072e5fbb.png?w=90&h=90" with='30' alt="" />
+      <img src="https://cn.i.cdn.ti-platform.com/content/1177/mao-mao/showpage/ar/showpicker.dcea7aca.png?w=90&h=90" with='30' alt="" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
